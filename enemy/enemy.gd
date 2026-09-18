2d2d0b813b3b6eac25fce34eacccc3cc6e9a9b08
Clifford William <ccliffordwilliam@gmail.com>
2026-09-18 19:39:00 +0700

    feat: add smoke particle when enemy disappear when defeated

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -7,7 +7,11 @@ const SQUISH_IMPACT_DURATION := 0.1
 const SQUISH_SCALE := Vector2(1.2, 0.5)
 const SQUISH_FROM_SCALE := Vector2(1.5, 0.2)
 
+const SmokeParticlesScene := preload("res://enemy/smoke_particles.tscn")
+
+
 @onready var state_machine: StateMachine = $StateMachine
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 var direction := 1.0
 
@@ -27,4 +31,11 @@ func play_squish() -> void:
 	_squash_tween.tween_property(sprite, "scale", SQUISH_FROM_SCALE, SQUISH_IMPACT_DURATION) \
 	.set_trans(Tween.TRANS_EXPO)
 	_squash_tween.tween_property(sprite, "scale", SQUISH_SCALE, SQUISH_DURATION)
-	_squash_tween.tween_callback(queue_free)
+	_squash_tween.tween_callback(_die)
+
+
+func _die() -> void:
+	var smoke: SmokeParticles = SmokeParticlesScene.instantiate()
+	smoke.global_position = collision_shape.global_position
+	get_parent().add_child(smoke)
+	queue_free()
---
+++ enemy/smoke_particles.gd
@@ -0,0 +1,7 @@
+class_name SmokeParticles
+extends GPUParticles2D
+
+
+func _ready() -> void:
+	finished.connect(queue_free)
+	emitting = true
